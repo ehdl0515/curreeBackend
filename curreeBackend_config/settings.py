79e20dfe5d368e52b880a0fad5695b4d3551f7db
KDH <ehdl0515@naver.com>
2024-04-03 15:48:27 +0900

    [MOD (settings)] static 관련 경로 추가

--- curreeBackend_config/settings.py
+++ curreeBackend_config/settings.py
@@ -134,6 +134,9 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
